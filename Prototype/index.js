265f072137aa13cb607a3db94354d98f5a94b35f
Md_Asgor_Ali <asgor.ice@gmail.com>
2021-02-09 00:55:47 +0600

    Instance and prototype

--- Prototype/index.js
+++ Prototype/index.js
@@ -1,11 +1,29 @@
-function Person(name){
-	this.name = name
+//function Person(name){
+//	this.name = name
+//}
+
+//Person.prototype.PI = 3.1416
+
+//var p1 = new Person("Md. A A A Ali")
+//var p2 = new Person("Asgor Ali")
+
+//console.log(p1)
+//console.log(p2)
+
+
+function Square(width){
+	this.width = width
 }
 
-Person.prototype.PI = 3.1416
+Square.prototype = {
+	draw: function(){
+		console.log("draw")
+	},
+	toString: function(){
+		return 'My width is = '+ this.width
+	}
+}
 
-var p1 = new Person("Md. A A A Ali")
-var p2 = new Person("Asgor Ali")
 
-console.log(p1)
-console.log(p2)
+var sqr1 = new Square(10)
+var sqr2 = new Square(5)
